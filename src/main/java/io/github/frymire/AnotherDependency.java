8e9ed6165c4b0993cb45c68054778d306a2b825d
Frymire, Mark E. <mark.frymire@dac.us>
2016-06-06 12:28:13 -0400

    Add a null JMockit example.

---
+++ src/main/java/io/github/frymire/AnotherDependency.java
@@ -0,0 +1,17 @@
+// UNCLASSIFIED
+
+package io.github.frymire;
+
+/**
+ * class_comment_here<p>
+ *
+ * (C) Copyright DECISIVE ANALYTICS Corporation 2016<br>
+ * All Rights Reserved<br>
+ * DAC Proprietary<br>
+ *
+ * @author mark.e.frymire
+ *
+ */
+public class AnotherDependency {
+
+}
